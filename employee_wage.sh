1f1e0c88620142a85fd7474d69c793722038aa7a
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-01 19:05:22 +0530

    added employee_wage.sh

--- employee_wage.sh
+++ employee_wage.sh
@@ -2,20 +2,25 @@
 
 random=$(( $RANDOM%2 ))
 hour_wage=20
-
+monthly_days=0
+monthly_wage=0
 case $random in
 
   0)
    echo "employee is absent"
    echo "dialy wage = 0" ;;
 
-  1) work_hours=$(( $RANDOM%8 ))
-    if [ $work_hours -eq 7 ]
-     then
-       echo "employee is present full_time"
-       echo "dialy wage = "$(( 8*$hour_wage ))
-     else
-       echo "employee is present part_time"
-       echo "employee wage = "$(( ($work_hours+1)*$hour_wage ))
-     fi  ;;
+  1)  while [ $monthly_days -lt 20 ]
+      do
+        monthly_days=$(( $monthly_days+1 ))
+        work_hours=$(( $RANDOM%8 ))
+        if [ $work_hours -eq 7 ]
+        then
+            monthly_wage=$(( $monthly_wage+(8*$hour_wage) ))
+        else
+             monthly_wage=$(( $monthly_wage+($(( ($work_hours+1)*$hour_wage )) ) ))
+        fi
+      done  ;;
+      
  esac
+echo $monthly_wage

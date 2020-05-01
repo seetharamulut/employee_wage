861ecbc3f433563c3c00ad54f66f4d8e9c91e196
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-01 18:48:52 +0530

    added employee_wage.sh

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,20 +1,21 @@
-#! /bin/bash/ -x
+#! /bin/bash/ 
 
 random=$(( $RANDOM%2 ))
 hour_wage=20
 
-if [ $random -eq 1 ]
-then
-  work_hours=$(( $RANDOM%8 ))
-   if [ $work_hours -eq 7 ]
-   then
-      echo "employee is present full_time"
-      echo "dialy wage = "$(( 8*$hour_wage ))
-   else
-      echo "employee is present part_time"
-      echo "employee wage = "$(( ($work_hours+1)*$hour_wage ))
-   fi
-else
+case $random in
+
+  0)
    echo "employee is absent"
-   echo "dialy wage = 0"
-fi
+   echo "dialy wage = 0" ;;
+
+  1) work_hours=$(( $RANDOM%8 ))
+    if [ $work_hours -eq 7 ]
+     then
+       echo "employee is present full_time"
+       echo "dialy wage = "$(( 8*$hour_wage ))
+     else
+       echo "employee is present part_time"
+       echo "employee wage = "$(( ($work_hours+1)*$hour_wage ))
+     fi  ;;
+ esac

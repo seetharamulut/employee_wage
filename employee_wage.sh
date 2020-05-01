a3301e854d2a9d8b3c818f34c9cd874699fd1679
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-01 19:46:11 +0530

    added employee_wage.sh

--- employee_wage.sh
+++ employee_wage.sh
@@ -2,29 +2,36 @@
 
 random=$(( $RANDOM%2 ))
 hour_wage=20
+work_hours=0
 monthly_days=0
 monthly_wage=0
 total_hours=0
+
+function workhours(){
+ echo $(( $monthly_wage/20 ))
+}
+
 case $random in
 
   0)
    echo "employee is absent"
    echo "dialy wage = 0" ;;
 
-  1)  while [ $monthly_days -lt 20 ] && [ $total_hours -lt 100 ]
+  1)  while [ $monthly_days -lt 20 ] && [ $work_hours -lt 100 ]
       do
         monthly_days=$(( $monthly_days+1 ))
-        work_hours=$(( $RANDOM%8 ))
-        total_hours=$(( $total_hours+($work_hours+1) ))
+        work_hours=$(( $work_hours+$RANDOM%8 ))
+       # total_hours=$(( $total_hours+($work_hours+1) ))
       done
-        if [ $total_hours -gt 100 ]
+        if [ $work_hours -gt 100 ]
         then
-            total_hours=100
-            monthly_wage=$(( $total_hours*$hour_wage ))
+            work_hours=100
+            monthly_wage=$(( $work_hours*$hour_wage ))
         else
-             monthly_wage=$(( $total_hours*$hour_wage ))
+             monthly_wage=$(( $work_hours*$hour_wage ))
         fi  ;;
       
  esac
+total_hours=$( workhours $monthly_wage )
 echo "employee present hours in month : "$total_hours
 echo "employee monthly_wage is :" $monthly_wage
